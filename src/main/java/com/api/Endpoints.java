43869661e8b5003d1d2a6043a2159b1531f255a6
Viktoria <viks99sp@abv.bg>
2025-09-06 14:04:38 +0300

    initial commit

--- src/main/java/com/api/Endpoints.java
+++ src/main/java/com/api/Endpoints.java
@@ -1,8 +1,12 @@
 package com.api;
 
 public class Endpoints {
-    String VEHICLE_ENDPOINT = "/vehicles";
-    String SERVICE_ENDPOINT = "/services";
-    //This is just a text
-    //Just a text
+    private final String GET_USER = "/api/user/{id}";
+    private final String GET_ALL_USERS = "/api/users";
+    private final String UPDATE_USER = "/api/users/customers";
+    private final String DELETE_USER = "/api/users/{id}";
+    private final String GET_USER_BY_USERNAME = "/api/users?username={username}&sortDirection={sort}&page={page}&size={size}";
+    private final String GET_USER_BY_EMAIL = "/api/users?email={email}&sortDirection={sort}&page={page}&size={size}";
+    private final String GET_USER_BY_PHONE_NUMBER = "/api/users?phoneNumber={phone}&sortDirection={sort}&page={page}&size={size}";
+    private final String GET_USER_BY_BRAND = "/api/users?vehicleBrand={brand}&sortDirection={sort}&page={page}&size={size}";
 }
