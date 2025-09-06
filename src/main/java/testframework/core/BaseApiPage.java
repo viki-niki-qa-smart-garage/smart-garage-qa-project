7c9a21216e2800408b7d235ecc973c3676efacaf
Viktoria <viks99sp@abv.bg>
2025-09-06 13:03:41 +0300

    initial commit

--- src/main/java/testframework/core/BaseApiPage.java
+++ src/main/java/testframework/core/BaseApiPage.java
@@ -2,34 +2,5 @@ package testframework.core;
 
 public class BaseApiPage {
 
-//    private final String baseUrl;
-//    private final String servicePath;
-//    private final Gson serializer;
-//    private RequestSpecification requestSpecification;
-//
-//    public BaseApiService(String servicePath, String baseUrl) {
-//        this.baseUrl = baseUrl;
-//        this.servicePath = servicePath;
-//
-//        // configure gson serializer
-//        GsonBuilder builder = new GsonBuilder();
-//        builder.setPrettyPrinting();
-//        serializer = builder.create();
-//    }
-//
-//    public String getServiceUrl() {
-//        return baseUrl + servicePath;
-//    }
-//
-//    public RequestSpecification request() {
-//        return requestSpecification;
-//    }
-//
-//    protected Gson gson() {
-//        return serializer;
-//    }
-//
-//    protected void setRequestSpecification(RequestSpecification req) {
-//        requestSpecification = req;
-//    }
+
 }
