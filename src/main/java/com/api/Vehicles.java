81eba01f318b61d65b0614d57e04cba31d0eb111
Nikolay V. <nsoul1990@gmail.com>
2025-09-07 13:13:23 +0300

    initial commit

--- src/main/java/com/api/Vehicles.java
+++ src/main/java/com/api/Vehicles.java
@@ -1,8 +1,48 @@
 package com.api;
 
 public class Vehicles {
-    String brand;
-    String model;
-    String engineType;
-    int year;
+   private String brandName;
+   private String modelName;
+   private int year;
+   private String engineType;
+
+
+    public Vehicles(String brandName, String modelName, int year, String engineType) {
+        this.brandName = brandName;
+        this.modelName = modelName;
+        this.year = year;
+        this.engineType = engineType;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
 }
